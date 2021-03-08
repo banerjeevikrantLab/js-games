1aea68e6d98b94a4935e8a4b48b3483d2ce1de0e
Vikrant Banerjee (Zaun) <66473930+banerjeevikrantLab@users.noreply.github.com>
2021-03-08 14:32:17 -0600

    initial commit

---
+++ crazysnakegame.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+	<head>
+		<title>Crazy Snake Game</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+		<link rel="stylesheet" href = "../css/template.css">
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+		<script type="text/javascript" src="gameslib.js"></script>
+		<style>
+			body{
+				background-color: lightblue;
+			}
+			.canvas{
+				border: 3px solid black;
+				top: 50px;
+				position: relative;
+				left: 10%;
+			}
+		</style>
+	</head>
+	
+	
+	<body>
+		<h1 class = "game-heading">Crazy Snake Game</h1>
+
+<canvas
+width="400"
+height="500"
+class="canvas"></canvas>
+
+<script>
+window.onload = function() {
+
+    
+
+
+var POINTS_TEXT_X = 5;
+var POINTS_TEXT_Y_BUFFER = 5;
+var POINTS_FONT = "17pt Arial";
+
+var scoreLabel = new Text(0, POINTS_FONT);
+var score = 0;
+
+var FOOD_DELAY = 6000;
+var FOOD_RADIUS = 5;
+var FOOD_COLOR = Color.red;
+
+var snake;
+var SNAKE_DIM = 10;
+var SNAKE_COLOR = Color.green;
+var spSnake = SNAKE_DIM / 9;
+
+var NORTH = 0;
+var EAST = 1;
+var SOUTH = 2;
+var WEST = 3;
+
+var direction = EAST;
+
+var DELAY = 80;
+
+function start(){
+    drawSnake();
+    keyDownMethod(keyPress);
+    
+    setTimer(moveSnake, DELAY);
+    setTimer(addFood, FOOD_DELAY);
+    
+    scoreLabel.setPosition(0 + POINTS_TEXT_X, getHeight() - POINTS_TEXT_Y_BUFFER);
+    add(scoreLabel);
+    
+}
+
+function drawSnake(){
+    snake = new Rectangle(SNAKE_DIM, SNAKE_DIM);
+    snake.setPosition(getWidth()/2, getHeight()/2);
+    snake.setColor(SNAKE_COLOR);
+    add(snake);
+}
+
+function keyPress(e){
+    if (e.keyCode == Keyboard.UP){
+        direction = NORTH;
+    }
+    if (e.keyCode == Keyboard.DOWN){
+        direction = SOUTH;
+    }
+    if (e.keyCode == Keyboard.LEFT){
+        direction = WEST;
+    }
+    if (e.keyCode == Keyboard.RIGHT){
+        direction = EAST;
+    }
+}
+
+function moveSnake(){
+    var trail = new Rectangle(SNAKE_DIM, SNAKE_DIM);
+	trail.setPosition(snake.getX(), snake.getY());
+	trail.setColor(SNAKE_COLOR);
+	add(trail);
+	
+	var newX = snake.getX();
+	var newY = snake.getY();
+    if(direction == NORTH){
+        newY -= SNAKE_DIM;
+    } 
+    if(direction == SOUTH){
+        newY += SNAKE_DIM;
+    } 
+    if(direction == WEST){
+        newX -= SNAKE_DIM;
+    } 
+    if(direction == EAST){
+        newX += SNAKE_DIM;
+    } 
+    newX =  (newX + getWidth()) % getWidth();
+    newY =  (newY + getHeight()) % getHeight();
+    var elem =  getElementAt(newX+5, newY+5);
+	if(elem != null) {
+	    if (elem.getType() == 'Rectangle') {
+            youLose();
+            return;
+	    } else if (elem.getType() == 'Circle') {
+	        score += 100;
+	        remove(elem);
+	    }
+    }else{
+        score += 1;
+    }
+    snake.setPosition(newX, newY);
+    
+    scoreLabel.setText(score);
+}
+
+
+function youLose(){
+    var text = new Text("You Lose.", "40pt Arial");
+    text.setPosition(70, 300);
+    add(text);
+    stopTimer(moveSnake);
+    stopTimer(addFood);
+}
+
+function addFood(){
+    var possFood = false;
+    while(possFood == false){
+        var numx = Randomizer.nextInt(1 + FOOD_RADIUS, getWidth() - FOOD_RADIUS);
+        var numy = Randomizer.nextInt(1 + FOOD_RADIUS, getHeight() - FOOD_RADIUS);
+        var x = numx  - (numx % 10) + 5;
+        var y = numy  - (numy % 10) + 5;
+        
+        if(getElementAt(x, y) == null){
+            possFood = true;
+            var food = new Circle(FOOD_RADIUS);
+            food.setColor(FOOD_COLOR);
+            food.setPosition(x, y);
+            
+            add(food);
+        }
+    }
+    
+}
+
+
+
+    if (typeof start === 'function') {
+        start();
+    }
+};
+</script>
+	</body>
+	
+	
+</html>
---
+++ lucknumbergame.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+
+<html lang="en">	
+
+	<head>
+		<title>Lucky Number Game</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+		<link rel="stylesheet" href = "../css/template.css">
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+		<script type="text/javascript" src="gameslib.js"></script>
+		<style>
+			body{
+				background-color: lightblue;
+			}
+			.canvas{
+				border: 3px solid black;
+				top: 50px;
+				position: relative;
+				left: 10%;
+			}
+		</style>
+	</head>
+	
+	
+	<body>
+		<h1 class = "game-heading">Lucky Number Game/h1>
+
+<canvas
+width="400"
+height="500"
+class="canvas"></canvas>
+
+<script>
+window.onload = function() {
+
+    
+var clicktostart;
+var chances = 1;
+var numGuess;
+var num1;
+var num2;
+var corrnum;
+var num;
+var howManyMove;
+
+function main(){
+    setUp();
+    mouseClickMethod(baseSetUp);
+}
+function setUp(){
+    setBackgroundColor(Color.black);
+    var heading = new Text("Lucky Number", "30pt Arial");
+    heading.setColor(Color.red);
+    heading.setPosition(getWidth()/8, getHeight()/7);
+    add(heading);
+    clicktostart = new Text("Click To Start!", "30pt Arial");
+    clicktostart.setColor(Color.red);
+    clicktostart.setPosition(getWidth()/7, getHeight()/2);
+    add(clicktostart);
+}
+function baseSetUp(e){
+    
+    remove(clicktostart);
+    num1 = readInt("So? From? ");
+    num2 = readInt("To? ");
+    corrnum = Randomizer.nextInt(num1, num2);
+    numGuess = new Text("OK! Click to Start!", "30pt Arial");
+    numGuess.setColor(Color.red);
+    numGuess.setPosition(getWidth()/11, getHeight()/2);
+    add(numGuess);
+    mouseClickMethod(systemBase);
+    
+}
+function systemBase(e){
+    num = readInt("Guess a number");
+    while(num != corrnum){
+    	if(num > corrnum){
+    		num = readInt("Too Large! Try Guessing Another number");
+    	}
+    	if(num < corrnum){
+    		num = readInt("Too Small! Try Guessing Another number");
+    	}
+        
+        chances++;
+    }
+    if (num == corrnum){
+        youWin();
+    }
+}
+function youWin(){
+    remove(numGuess);
+    var success = new Text("You got it!", "30pt Arial");
+    success.setColor(Color.red);
+    success.setPosition(getWidth()/2 - success.getWidth()/2, getHeight()/3);
+    add(success);
+	if (chances == 1){
+		howManyMove = new Text("It took you 1 chance to figure it out!", "18pt Arial");
+		howManyMove.setColor(Color.red);
+		howManyMove.setPosition(getWidth()/2 - howManyMove.getWidth()/2, getHeight()/2);
+		add(howManyMove);
+	}else{
+		howManyMove = new Text("It took you " + chances +" chances to figure it out!", "18pt Arial");
+		howManyMove.setColor(Color.red);
+		howManyMove.setPosition(getWidth()/2 - howManyMove.getWidth()/2, getHeight()/2);
+		add(howManyMove);
+	}
+	var startAgain = new Text("Start Again? Click!", "25pt Arial");
+	    startAgain.setColor(Color.red);
+	    startAgain.setPosition(getWidth()/2 - startAgain.getWidth()/2, getHeight()* 5/6);
+	    add(startAgain);
+	if(chances == 1){
+	        var text = new Text("In one Chance! RU INSIDE MY Operating System!!?", "12pt Arial");
+	        text.setColor(Color.red);
+	        text.setPosition(getWidth()/2 - text.getWidth()/2, getHeight()*4/6);
+	        add(text);
+    	}
+    if(chances <= 5 && chances != 1){
+        var text = new Text("Wow! You are quick!", "15pt Arial");
+        text.setColor(Color.red);
+        text.setPosition(getWidth()/2 - text.getWidth()/2, getHeight()*4/6);
+        add(text);
+    }
+	
+	mouseClickMethod(restart);
+	
+}
+function restart(e){
+    removeAll();
+	chances = 1;
+	main();
+}
+
+
+
+    if (typeof main === 'function') {
+        main();
+    }
+};
+</script>
+	</body>
+	
+	
+</html>
