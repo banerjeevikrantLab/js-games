48f208b75c4bc88cc0f9e1ed0158e3e9323f27d8
Vikrant Banerjee (Zaun) <66473930+banerjeevikrantLab@users.noreply.github.com>
2021-03-11 14:21:45 -0600

    edits

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
-welcome
-choose games
+<!DOCTYPE html>
+
+<html>
+
+<body>
+
+<a href="helicoptergame.php" target="_blank">Helicopter Game</a>
+<a href="breakoutgame.php" target="_blank">Breakout Game</a>
+<a href="tictactoegame.php" target="_blank">Tic Tac Toe Game</a>
+<a href="crazysnakegame.php" target="_blank">Crazy Snake Game</a>
+<a href="luckynumbergame.php" target="_blank">Lucky Number Game</a>
+<a href="bouncyballgame.php" target="_blank">Bouncy Balls Game</a>
+
+</body>
+
+
+</html>
